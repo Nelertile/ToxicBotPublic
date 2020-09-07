9cac7ab1b95b305b9d8d6f999b3386bde015cfea
NotToxic <47497223+Swegajg09@users.noreply.github.com>
2020-09-07 18:33:56 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const server = express();
 server.all('/', (req, res)=>{
-    res.send('Your bot is alive!')
+    res.send('NTBE Core Handled all requests and launched Toxic Bot.')
 })
 function keepAlive(){
     server.listen(3000, ()=>{console.log("server running")});
